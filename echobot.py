ef61fc1cd439e4e821ba266e620c67e9fbca2bec
Silas Kinnear <silas.kinnear@gmail.com>
2026-03-09 13:22:58 -0700

    a

--- echobot.py
+++ echobot.py
@@ -1,4 +1,6 @@
 import streamlit as st 
+import numpy as np 
 
-with st.chat_message("user"):
-    st.write("Hello!")
+with st.chat_message("assistant"):
+    st.write("Hello human")
+    st.bar_chart(np.random.rand(30,3))
